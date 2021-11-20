e7d3fecb24fe06e348e09ec4cc4fccfee39a6e65
Monu.Vishwakarma <monuvishwakarmabpl@gmail.com>
2021-11-20 18:29:12 +0530

    Updates

---
+++ precache-manifest.55334bda1013213f97802f8c46fa2931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50db594b7614f24b961deefdb0e3757e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f50279e742ed5fba720",
+    "url": "/static/css/main.56c7d002.chunk.css"
+  },
+  {
+    "revision": "9c53ec42afbab32ba622",
+    "url": "/static/js/2.b7263f78.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b7263f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f50279e742ed5fba720",
+    "url": "/static/js/main.d1f6027f.chunk.js"
+  },
+  {
+    "revision": "d266a85b6a51200b4858",
+    "url": "/static/js/runtime-main.066e25ad.js"
+  }
+]);
